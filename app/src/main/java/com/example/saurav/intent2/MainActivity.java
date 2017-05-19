eae384b59940e1d0ab356f410558d93abdd5df10
Saurav Yadav <sauravyadav1286@gmail.com>
2017-05-19 23:43:32 +0530

    Android_Session7Assignment2
    Android_Session7Assignment2 Description

---
+++ app/src/main/java/com/example/saurav/intent2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.saurav.intent2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.btn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent contact = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+
+                startActivityForResult(contact,RESULT_OK);
+            }
+        });
+    }
+
+}
